04cffd05470003c199e56bd5d47b08d44edf3b89
sbrad33 <sbradley123@gmail.com>
2025-03-26 10:13:03 -0400

    Initial Commit

---
+++ Shawn_Bradley_Portolio/js/scripts.js
@@ -0,0 +1,43 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    //possible to have reusable code here to collapse/hide or expand/show the videos under each section?
+    const linkedMediaList = document.body.querySelector('linked-media-videos');
+    var btn = document.getElementById('linked-media-toggle-btn');
+    btn.addEventListener('click', () =>{
+        console.log("button clicked");
+        
+    });
+
+});
+
